b0705102aee5847cfaf61caa0bca4bb712ae17c5
NTIDavid <david.andersson@ntig.se>
2023-01-24 16:29:01 +0100

    next

--- index.php
+++ index.php
@@ -5,10 +5,34 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Testprojekt</title>
+<script>
+let data = [
+	{
+		name: "Kalle",
+		age: 17
+	},{
+		name: "David",
+		age: 34
+	},{
+		name: "Noor",
+		age: 16,
+		bd: "April"
+	},{
+		name: "Åke",
+		age: 65
+	}
+];
+data.sort(function(a, b) {
+	if(b.name == "Noor") {
+		return 1;
+	} else {
+		return a.name.localeCompare(b.name);
+	}
+});
+console.log(data);
+</script>
 </head>
 <body>
-<?php
-echo rand(0, 5);
-?>
+
 </body>
 </html>
